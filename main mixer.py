9e25344676f9fed17b70843afa885f631f65a5b9
Jaiden-Baynes <baynesj@mcmaster.ca>
2025-12-24 15:18:47 -0500

    add to do list, add global variable usage , changed it to play indefinitely swapping between instrumental songs until manually stopped

--- main mixer.py
+++ main mixer.py
@@ -1,9 +1,19 @@
+#To Add:
+# Modes, different mode functions so that when the song ends a different mode function is called either to end or pick a new song
+# If not in folder try another song. IF retries exists song number then quit
+
 # Music Player with Instrumental and Lyrical Swap
 
 import pygame
 import random
 import os
 import time
+
+# Define key global variables
+elapsed_time = 0
+current_track = ['His World (Instrumental).mp3','His World -Theme of Sonic The Hedgehog.mp3']
+swap_occurred = False
+randoSwap = 0  
 
 # Define the custom event type for when music ends
 MUSIC_END_EVENT = pygame.USEREVENT + 1
@@ -20,14 +30,13 @@ PLAYLIST = {
 PLAYLIST = [['Live and Learn (Instrumental) - Sonic Adventure 2 [OST].mp3', 'Live and Learn - Sonic Adventure 2 [OST].mp3'], ['Escape From the City (Instrumental) - Sonic Adventure 2 [OST].mp3', 'Escape From the City (City Escape) - Sonic Adventure 2 [OST].mp3'], ['Can You Feel The Sunshine - Sonic R (Instrumental).mp3','Can You Feel the Sunshine - Sonic R [OST].mp3']]
 
 song4 = ['All Hail Shadow (Crush 40 Ver.) (Instrumental).mp3','All Hail Shadow -Theme of Shadow The Hedgehog.mp3']
-song5 = ['His World (Instrumental).mp3','His World -Theme of Sonic The Hedgehog.mp3']
-song6 = ['With Me (Instrumental Version) - Sonic and the Black Knight [OST].mp3', 'With Me - Sonic and the Black Knight [OST].mp3']
-
-
 PLAYLIST.append(song4)
+song5 = ['His World (Instrumental).mp3','His World -Theme of Sonic The Hedgehog.mp3']
 PLAYLIST.append(song5)
+song6 = ['With Me (Instrumental Version) - Sonic and the Black Knight [OST].mp3', 'With Me - Sonic and the Black Knight [OST].mp3']
 PLAYLIST.append(song6)
-
+song7 = ['Catch Me If You Can (Instrumental).mp3','Catch Me If You Can.mp3']
+PLAYLIST.append(song7)
 
 def select_random_track():
     """Randomly selects a song's instrumental version"""
@@ -36,45 +45,59 @@ def select_random_track():
 
 def play_next_track():
     """Loads and plays the instrumental version of the currently selected track. track."""
+    global next_track
     next_track = select_random_track()
     pygame.mixer.music.load(next_track[0])
     pygame.mixer.music.play()
     print(f"Now playing: {next_track[0]}")
     return next_track
 
+def reset():
+    minTime = 10
+    maxTime = 60
+    global current_track
+    current_track = play_next_track()
+    elapsed_time = 0
+    global swap_occurred 
+    swap_occured = False
+    global randoSwap
+    randoSwap = random.uniform(minTime, maxTime)  # Random time to swap
+    print(randoSwap)    
+
 def main_music_player():
     pygame.mixer.init()
     pygame.init()
+    
+    global elapsed_time
+    global current_track
+    global swap_occurred
+    global randoSwap
 
     pygame.mixer.music.set_endevent(MUSIC_END_EVENT)
-
-    current_track = play_next_track()
-    elapsed_time = 0
-    swap_occurred = False
-    randoSwap = 5 #random.uniform(15, 120)  # Random time to swap
-    print(randoSwap)
+    reset() #selects new Instrumental
 
     running = True
     while running:
-        if not swap_occurred:
+        if swap_occurred == False:
             elapsed_time = pygame.mixer.music.get_pos() / 1000  # Get elapsed time in seconds    
-            print(randoSwap - elapsed_time)            
+            #print(randoSwap - elapsed_time)            
             if elapsed_time >= randoSwap:
                 pygame.mixer.music.load(current_track[1])
                 pygame.mixer.music.play(0, elapsed_time)  # Resume from where instrumental left off
-                #print(f"Now playing: {current_track[1]}")
+                print(f"Now playing: {current_track[1]}")
                 swap_occurred = True   
         
         for event in pygame.event.get():
             if event.type == MUSIC_END_EVENT:
-                running = False
+                reset()            
+                #running = False
             if event.type == pygame.QUIT:
                 running = False
 
         time.sleep(0.1)  # Short delay before checking for events
         pygame.time.Clock().tick(10)
 
-    pygame.quit()
-
+    pygame.quit() 
+    
 if __name__ == '__main__':
     main_music_player()
